4f525217730f1c5bfec24921a0928d1326a80ba3
Martin Moss <martyzz1@gmail.com>
2020-01-21 16:04:33 +0000

    tidy up and rename to be more explicit

--- yarn-system-husky/run-husky-commit.sh
+++ yarn-system-husky/run-husky-commit.sh
@@ -31,26 +31,5 @@ if [ "${HUSKY_SKIP_HOOKS}" = "true" ] || [ "${HUSKY_SKIP_HOOKS}" = "1" ]; then
   exit 0
 fi
 
-if [ "${HUSKY_USE_YARN}" = "true" ] || [ "${HUSKY_USE_YARN}" = "1" ]; then
-  debug "Calling husky through Yarn"
-  yarn husky-run $hookName "$gitParams"
-else
-
-  if ! command -v node >/dev/null 2>&1; then
-    echo "Info: can't find node in PATH, trying to find a node binary on your system"
-  fi
-
-  if [ -f "$scriptPath" ]; then
-    # if [ -t 1 ]; then
-    #   exec < /dev/tty
-    # fi
-    if [ -f ~/.huskyrc ]; then
-      debug "Sourcing '~/.huskyrc'"
-      . ~/.huskyrc
-    fi
-    node_modules/run-node/run-node "$scriptPath" $hookName "$gitParams"
-  else
-    echo "Can't find Husky, skipping $hookName hook"
-    echo "You can reinstall it using 'npm install husky --save-dev' or delete this hook"
-  fi
-fi
+debug "Calling husky through Yarn"
+yarn husky-run $hookName "$gitParams"
