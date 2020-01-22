3890b54cbe1ab2c4a793061193acd2fb8232e39a
Martin Moss <martyzz1@gmail.com>
2020-01-22 10:21:08 +0000

    grrr testing

--- yarn-system-husky/run-husky-commit.sh
+++ yarn-system-husky/run-husky-commit.sh
@@ -11,6 +11,46 @@
 #   Homepage: https://github.com/typicode/husky#readme
 
 echo $@
+echo "hmmmm"
+echo "$#"
+
+OPTIONS=()
+# If arg doesn't pass [ -f ] check, then it is assumed to be an option
+#
+while [ $# -gt 0 ] && [ "$1" != "-" ] && [ "$1" != "--" ] && [ ! -f "$1" ]; do
+	OPTIONS+=("$1")
+	shift
+done
+
+FILES=()
+# Assume start of file list (may still be options)
+#
+while [ $# -gt 0 ] && [ "$1" != "-" ] && [ "$1" != "--" ]; do
+	FILES+=("$1")
+	shift
+done
+
+# If '--' next, then files = options
+#
+if [ $# -gt 0 ]; then
+	if [ "$1" == "-" ] || [ "$1" == "--" ]; then
+		shift
+		# Append to previous options
+		#
+		OPTIONS=("${OPTIONS[@]}" "${FILES[@]}")
+		FILES=()
+	fi
+fi
+
+# Any remaining arguments are assumed to be files
+#
+while [ $# -gt 0 ]; do
+	FILES+=("$1")
+	shift
+done
+
+echo "OPTIONS \n$OPTIONS"
+echo "FILES \n$FILES"
 
 scriptPath="node_modules/husky/run.js"
 hookName="pre-commit"
